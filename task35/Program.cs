ea5666bbb1886077380eb477737eb92aaef11ce5
mikle <mr.mikle@mail.ru>
2022-09-30 10:24:54 +0300

    jobs done2

---
+++ task35/Program.cs
@@ -0,0 +1,42 @@
+﻿// 35
+Console.Clear();
+
+int[] CreateArray()
+{
+
+    int[] array = new int[123];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} |");
+    }
+}
+
+int Count(int[] AnyArray)
+{
+    int count = 0;
+    for (int i = 0; i < AnyArray.Length; i++)
+    {
+        if (AnyArray[i] > 9 & AnyArray[i] < 100)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+int[] arr1 = CreateArray();
+PrintArray(arr1);
+Console.WriteLine();
+int count = Count(arr1);
+Console.WriteLine(count);
+
+
---
+++ task37/Program.cs
@@ -0,0 +1,67 @@
+﻿// 37
+Console.Clear();
+int[] CreateArray()
+{
+    //Random rnd = new Random();  
+    //int value = rnd.Next(10);  //Получить очередное случайное число
+
+    int[] array = new int[new Random().Next(3, 8)]; //здесь регулируем количество элементов массива
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} |");
+    }
+}
+
+int[] ProisvArray(int[] AnyArray)
+{
+
+    // куча переменных для помощи и ветвлений
+    int lenarray = AnyArray.Length - 1;
+    int ostatok = AnyArray.Length % 2;
+    int lenNewArray = -1;
+    int Polovinka = AnyArray.Length / 2;
+    int lencikla = -1;
+
+    // определяем длинну нового массива и длинну цикла заполнения исходя из четности длинны первого массива
+    if (ostatok > 0)
+    {
+        lenNewArray = Polovinka + 1;
+        lencikla = Polovinka;
+    }
+    else
+    {
+        lenNewArray = Polovinka;
+        lencikla = Polovinka;
+    }
+    //объявляем новый массив
+    int[] NewArray = new int[lenNewArray];
+
+    //Если массив из нечетных элементов, то заполним сначало последний элемент нового массива
+    if (ostatok > 0) NewArray[Polovinka] = AnyArray[Polovinka];
+
+    //заполним элементы нового массива
+    for (int i = 0; i < lencikla; i++)
+    {
+        NewArray[i] = AnyArray[i] * AnyArray[lenarray];
+        lenarray--;
+    }
+
+    return NewArray;
+}
+
+int[] arr1 = CreateArray();
+PrintArray(arr1);
+Console.WriteLine();
+
+int[] arr2 = ProisvArray(arr1);
+PrintArray(arr2);
+
+
